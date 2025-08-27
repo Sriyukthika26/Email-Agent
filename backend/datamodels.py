42acb7a895db7edf5a9784a69bd7da4852c03881
Sriyukthika26 <23je0967@iitism.ac.in>
2025-08-27 20:05:27 +0530

    updated data models

--- backend/datamodels.py
+++ backend/datamodels.py
@@ -1,13 +1,16 @@
-# /models.py
+# /datamodels.py
 from typing import TypedDict, List, Literal
 from pydantic import BaseModel, Field
 
-# --- Pydantic Models for API Validation ---
+class EmailDraft(BaseModel):
+    """A generated email draft with a subject and body."""
+    subject: str = Field(description="The subject line of the email.")
+    body: str = Field(description="The body content of the email, written in a personalized and professional tone.")
 
 class GenerationRequest(BaseModel):
     """Request model for starting a new email generation flow."""
-    leadId: str
-    userId: str
+    leadId: int
+    userId: int
     user_instructions: str | None = None
 
 class UpdateRequest(BaseModel):
@@ -16,20 +19,17 @@ class UpdateRequest(BaseModel):
     decision: Literal["regenerate", "approve"]
     feedback: str | None = None
 
-# --- LangGraph State Definition ---
-
-class EmailDraft(BaseModel):
-    """A generated email draft with a subject and body."""
-    subject: str = Field(description="The subject line of the email.")
-    body: str = Field(description="The body content of the email, written in a personalized and professional tone.")
+class StateUpdateRequest(BaseModel):
+    thread_id: str
+    email_history: List[EmailDraft]
 
 class AgentState(TypedDict):
     """Represents the state of our email generation agent."""
-    lead_id: str
-    user_id: str
-    user_instructions: str | None
+    lead_id: int
+    user_id: int
+    user_instructions: str
     db_data: dict
     email_history: List[EmailDraft]
-    feedback: str | None
-    human_decision: Literal["regenerate", "approve"]| None
-    error_message: str | None
+    feedback: str
+    human_decision: Literal["regenerate", "approve"]
+    error_message: str
