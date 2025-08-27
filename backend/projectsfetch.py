453a6eb03d07410d44109ce6838319d0a541eb65
Sriyukthika26 <23je0967@iitism.ac.in>
2025-08-27 21:24:56 +0530

    added projects fetch in backend

---
+++ backend/projectsfetch.py
@@ -0,0 +1,40 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+# Switch environment: "dev" or "staging"
+ENV = os.getenv("ENV", "staging")
+BASE_URL = f"https://{ENV}.api.buildmapper.ai"
+ENDPOINT = "/odoo/api/v1/companyProfile/projects/fetch/public/id"
+URL = BASE_URL + ENDPOINT
+
+# Headers 
+HEADERS = {
+    "Authorization": os.getenv("API_AUTH"),
+    "Content-Type": "application/json",
+    "test": "true"
+}
+
+def fetch_projects(company_id: int, page: int = 1, limit: int = 5, filters: dict = None):
+    payload = {
+        "companyId": company_id,
+        "page": page,
+        "limit": limit,
+        "filters": filters or {}
+    }
+
+    try:
+        response = requests.post(URL, headers=HEADERS, json=payload)
+
+        if response.status_code != 200:
+            print("Response text:", response.text)
+
+        response.raise_for_status()
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return None
